6aac0b78bbf3cb7fe73ca87005966d7ec9493acc
Scott Thrall <sthrall@night-market.com>
2023-05-08 11:03:10 -0700

    Fixing spacing issues in controller file.

--- ecom/storefront-reference-architecture/cartridges/app_custom_uhch/cartridge/controllers/LandingPage.js
+++ ecom/storefront-reference-architecture/cartridges/app_custom_uhch/cartridge/controllers/LandingPage.js
@@ -2,17 +2,17 @@ var PageMgr = require('dw/experience/PageMgr');
 var URLUtils = require('dw/web/URLUtils');
 
 exports.Show = function () {
-  var getId = (typeof request !== 'undefined') ? request.httpParameterMap.cid.stringValue : null; 
-  var page = PageMgr.getPage(getId);
-  var content = PageMgr.renderPage(page.ID, '');
+    var getId = (typeof request !== 'undefined') ? request.httpParameterMap.cid.stringValue : null;
+    var page = PageMgr.getPage(getId);
+    var content = PageMgr.renderPage(page.ID, '');
 
-  if (typeof response !== 'undefined') {
-    if (page != null && page.isVisible()) {
-      response.writer.print(content);
-    } else {
-      response.redirect(URLUtils.httpsHome().toString());
+    if (typeof response !== 'undefined') {
+      if (page != null && page.isVisible()) {
+        response.writer.print(content);
+      } else {
+        response.redirect(URLUtils.httpsHome().toString());
+      }
     }
-  }
 };
 
 exports.Show.public = true;
